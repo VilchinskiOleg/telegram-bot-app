3dafffceb54882ec8ee8e6e721dfe159be2bc3f8
VilchinskiOleg <vilchinskioleg@gmail.com>
2021-01-22 18:31:59 +0300

    Initial commit

---
+++ src/test/java/com/resliv/bot/TelegramBotAppTests.java
@@ -0,0 +1,13 @@
+package com.resliv.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
